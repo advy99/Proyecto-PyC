fd4bd164487f52c883a1ed7cb163137078fd19db
advy99 <advy99@gmail.com>
2017-03-17 22:53:22 +0100

    añadido ejercicio multiplicar matrices

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -11,61 +11,62 @@ function manual(){
 	ventanaManual=window.open('manual.html','','width=1000,height=500');
 }
 function ejSumaMatrices(){
-	ventanaSumaMatrices=window.open('ejercicios/ejSumaMatrices.html','','width=700,height=500')
+	ventanaSumaMatrices=window.open('ejercicios/ejSumaMatrices.html','','width=900,height=500')
 }
 function rellenarMatrices(){
 	//cogemosla matriz a y la rellenamos de numeros aleatorios
-	document.getElementsByClassName("a1")[0].innerHTML=Math.trunc((Math.random()*10));
-	document.getElementsByClassName("a1")[1].innerHTML=Math.trunc((Math.random()*10));
-	document.getElementsByClassName("a1")[2].innerHTML=Math.trunc((Math.random()*10));
-	document.getElementsByClassName("a2")[0].innerHTML=Math.trunc((Math.random()*10));
-	document.getElementsByClassName("a2")[1].innerHTML=Math.trunc((Math.random()*10));
-	document.getElementsByClassName("a2")[2].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("a")[0].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("a")[1].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("a")[2].innerHTML=Math.trunc((Math.random()*10));
+
+	document.getElementsByClassName("a")[3].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("a")[4].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("a")[5].innerHTML=Math.trunc((Math.random()*10));
+
+	document.getElementsByClassName("a")[6].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("a")[7].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("a")[8].innerHTML=Math.trunc((Math.random()*10));
 	
 	//cogemosla matriz b y la rellenamos de numeros aleatorios
 
-	document.getElementsByClassName("b1")[0].innerHTML=Math.trunc((Math.random()*10));
-	document.getElementsByClassName("b1")[1].innerHTML=Math.trunc((Math.random()*10));
-	document.getElementsByClassName("b1")[2].innerHTML=Math.trunc((Math.random()*10));
-	document.getElementsByClassName("b2")[0].innerHTML=Math.trunc((Math.random()*10));
-	document.getElementsByClassName("b2")[1].innerHTML=Math.trunc((Math.random()*10));
-	document.getElementsByClassName("b2")[2].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("b")[0].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("b")[1].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("b")[2].innerHTML=Math.trunc((Math.random()*10));
+
+	document.getElementsByClassName("b")[3].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("b")[4].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("b")[5].innerHTML=Math.trunc((Math.random()*10));
+
+	document.getElementsByClassName("b")[6].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("b")[7].innerHTML=Math.trunc((Math.random()*10));
+	document.getElementsByClassName("b")[8].innerHTML=Math.trunc((Math.random()*10));
 
 	document.getElementById("c11").value="";
 	document.getElementById("c12").value="";
 	document.getElementById("c13").value="";
+
 	document.getElementById("c21").value="";
 	document.getElementById("c22").value="";
 	document.getElementById("c23").value="";
+
+	document.getElementById("c31").value="";
+	document.getElementById("c32").value="";
+	document.getElementById("c33").value="";
 }
 function comprobarSuma(){
-	a1= new Array(3);
+	a1=new Array(8);
 	
-	a1=document.getElementsByClassName("a1");
+	a1=document.getElementsByClassName("a");
+	
+	b1= new Array(8);
 
-	a2= new Array(3);
+	b1=document.getElementsByClassName("b");
 
-	a2=document.getElementsByClassName("a2");
+	c1= new Array(8);
 
-	b1= new Array(3);
+	c1=document.getElementsByClassName("c");
 
-	b1=document.getElementsByClassName("b1");
-
-	b2= new Array(3);
-
-	b2=document.getElementsByClassName("b2");
-
-	c1= new Array(3);
-
-	c1=document.getElementsByClassName("c1");
-
-
-	c2= new Array(3);
-
-	c2=document.getElementsByClassName("c2");
-
-
-	for (var i = 0; i <= 2; i++) {
+	for (var i = 0; i <= 5; i++) {
 		if (c1[i].value==(Number(a1[i].innerHTML)+Number(b1[i].innerHTML))) {
 
 			c1[i].style.backgroundColor="green";
@@ -76,41 +77,26 @@ function comprobarSuma(){
 			c1[i].style.color="white";
 		}
 	}
-	for (var i = 0; i <= 2; i++) {
-		if (c2[i].value==(Number(a2[i].innerHTML)+Number(b2[i].innerHTML))) {
-			c2[i].style.backgroundColor="green";
-			c2[i].style.color="white";
-		}
-		else{
-			c2[i].style.backgroundColor="red";
-			c2[i].style.color="white";
-		}
-	}
 }
 function ejNumeroMatriz(){
-	ventanaNumeroMatriz=window.open('ejercicios/ejNumeroMatriz.html','','width=700,height=500')
+	ventanaNumeroMatriz=window.open('ejercicios/ejNumeroMatriz.html','','width=900,height=500')
 }
 function comprobarNumeroMatriz(){
-	a1=new Array(3);
-	a1=document.getElementsByClassName("a1");
-	b1= new Array(3);
+	a1=new Array(8);
+	
+	a1=document.getElementsByClassName("a");
+	
+	b1= new Array(8);
 
-	b1=document.getElementsByClassName("b1");
+	b1=document.getElementsByClassName("b");
 
-	b2= new Array(3);
+	c1= new Array(8);
 
-	b2=document.getElementsByClassName("b2");
-
-	c1= new Array(3);
-
-	c1=document.getElementsByClassName("c1");
+	c1=document.getElementsByClassName("c");
 
 
-	c2= new Array(3);
 
-	c2=document.getElementsByClassName("c2");
-
-	for (var i = 0; i <= 2; i++) {
+	for (var i = 0; i <= 5; i++) {
 		if (c1[i].value==(Number(a1[0].innerHTML)*Number(b1[i].innerHTML))) {
 
 			c1[i].style.backgroundColor="green";
@@ -121,14 +107,61 @@ function comprobarNumeroMatriz(){
 			c1[i].style.color="white";
 		}
 	}
-	for (var i = 0; i <= 2; i++) {
-		if (c2[i].value==(Number(a1[0].innerHTML)*Number(b2[i].innerHTML))) {
-			c2[i].style.backgroundColor="green";
-			c2[i].style.color="white";
+	
+}
+function ejMatrices(){
+	ventanaMatriz=window.open('ejercicios/ejMatrices.html','','width=900,height=500')
+}
+function comprobarMatrices(){
+	a1= new Array(8);
+	
+	a1=document.getElementsByClassName("a");
+
+	b1= new Array(8);
+	
+	b1=document.getElementsByClassName("b");
+	c1= new Array(8);
+	
+	c1=document.getElementsByClassName("c");
+	
+	sol=0;
+	sol=Number(sol);
+	j=0;
+	k=0;
+	cont=0;
+	num=0;
+	for (var l = 0; l <3; l++) {
+
+		for (var i = 0; i <=11; i++) {	
+			if(cont<3){
+				sol=sol+(Number(a1[num].innerHTML)*Number(b1[j].innerHTML));
+				j+=3;
+				cont++;
+				num++;
+			}
+			else{
+				if (c1[k].value==sol) {
+					c1[k].style.backgroundColor="green";
+					c1[k].style.color="white";
+					sol=0;
+				}
+				else{
+					c1[k].style.backgroundColor="red";
+					c1[k].style.color="white";
+					sol=0;
+				}
+				k++;
+				if(j>=11){
+					j-=11;
+				}
+				else{
+					j-=8;
+				}
+				cont=0;
+				num-=3;
+			}
+
 		}
-		else{
-			c2[i].style.backgroundColor="red";
-			c2[i].style.color="white";
-		}
+		num+=3;
 	}
 }
