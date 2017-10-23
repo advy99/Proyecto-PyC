0cad539428478c6ec59adb1f397e84d724b6182a
Andrew Banchich <andrewbanchich@gmail.com>
2017-10-23 15:10:28 -0400

    Update javascript.js
    adds example of how to use `querySelectorAll` to query multiple classes at once

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -49,19 +49,11 @@ const ejSumaMatrices = function(){
 //Funcion que rellena las matrices con numeros aleatorios y vacia automaticamente las casillas en los que se introducen los datos
 const rellenarMatrices = function(){
 	
-	//cogemosla matriz a y la rellenamos de numeros aleatorios
-	a=document.getElementsByClassName("a");
-	b=document.getElementsByClassName("b");
-	c=document.getElementsByClassName("c");
-	
-	//Rellena la matriz A
-	a.forEach(el => el.innerHTML=Math.trunc((Math.random()*10));
-
-	//Rellena la matriz B
-	b.forEach(el => el.innerHTML=Math.trunc((Math.random()*10)));
+	//Rellena la matriz A and B
+	document.querySelectorAll('.a, .b').forEach(el => el.innerHTML=Math.trunc((Math.random()*10));
 
 	//Vacia las casillas de resultados
-	c.forEach(el => el.value = "");
+	document.querySelector('.c').forEach(el => el.value = '');
 
 }
 
